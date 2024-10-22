a1f61e04db6b09f6582910560e0bc6874f8a21ea
Daniel Cronin <daniel@dcron.in>
2024-10-21 19:38:42 -0500

    add styling and turn off debug

--- epb_site/epb_site/settings.py
+++ epb_site/epb_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7a1kzhkmr)ioatvs#n77lg_gp^s42!qqdnot7vvj*(oc1$lor8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
